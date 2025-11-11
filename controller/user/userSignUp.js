7b5323dbc18f932a8d62773518ee45293a742312
Đinh Duy Thắng <thang2401204@gmail.com>
2025-11-11 14:06:03 +0700

    Cập nhật backend mới đường dẫn

--- controller/user/userSignUp.js
+++ controller/user/userSignUp.js
@@ -1,5 +1,5 @@
-const userModel = require("../models/userModel");
-const { sendOTP } = require("../utils/sendOTP");
+const userModel = require("../../models/userModel");
+const { sendOTP } = require("../../utils/sendOTP");
 const bcrypt = require("bcryptjs");
 
 const sendOtpToSignUpController = async (req, res) => {
